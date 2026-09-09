e04e38297f8d9c708e12169498e0956b54a4b318
Kauan Vinícius <kauan.francischini@aluno.senai.br>
2026-09-09 15:35:56 -0300

    Update investments.ts

---
+++ types/investments.ts
@@ -0,0 +1,13 @@
+export interface Investment {
+  initialValue: number;
+  anualProfitability: number;
+  years: number;
+}
+
+export interface InvestmentResult {
+  initialValue: number;
+  anualProfitability: number;
+  years: number;
+  finalValue: number;
+  profit: number;
+}
