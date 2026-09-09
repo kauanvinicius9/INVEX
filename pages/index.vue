fb9f17404909b075dbec43a4299efd500a25cc83
Francischini Kauan (CaP/ETS2) <frk7ca@bosch.com>
2026-09-09 09:12:45 -0300

    home screen

---
+++ pages/index.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import "./index.scss"
+</script>
+
+<template>
+    <main class="home">
+        <section class="home__hero">
+            <div class="home__content">
+                <span class="home__tag">Invex - Simule seus Investimentos</span>
+
+                <h1>
+                    Planeje de forma <span>simples</span>
+                </h1>
+
+                <p>
+                    Crie uma simulação, veja a evolução do seu patrimônio e 
+                    acompanhe os resultados através de um dashboard
+                </p>
+
+                <NuxtLink to="/dashboard" class="home__button">
+                    Iniciar simulação
+                </NuxtLink>
+            </div>
+
+            <div class="home__card">
+                <p>Exemplo de investimento</p>
+
+                <h2>R$ 12.000,00</h2>
+
+                <div class="home__info">
+                    <span>Rentabilidade anual</span>
+                    <strong>10%</strong>
+                </div>
+
+                <div class="home__info">
+                    <strong>Período</strong>
+                    <span>05 anos</span>
+                </div>
+
+                <div class="home__info">
+                    <strong>Valor estimado</strong>
+                    <span>R$ 19.326,12</span>
+                </div>
+            </div>
+        </section>
+    </main>
+</template>
