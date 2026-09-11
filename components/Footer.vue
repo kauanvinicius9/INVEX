ad45b8167ac29b30293818adcdf198c142d4a03b
Kauan Vinícius <kauan.francischini@aluno.senai.br>
2026-09-11 08:29:45 -0300

    Add footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,3 +1,13 @@
 <script setup lang="ts">
-import "./Footer.scss
+import "./Footer.scss";
 </script>
+
+<template>
+  <footer class="footer">
+    <div class="container footer__inner">
+      <p class="footer__copy">
+        &copy; {{ new Date().getFullYear() }} Invex. Todos os direitos reservados
+      </p>
+    </div>
+  </footer>
+</template>
